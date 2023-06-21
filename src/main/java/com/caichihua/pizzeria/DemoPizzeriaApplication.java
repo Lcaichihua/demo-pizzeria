8b70b8fac1ae1822a558056922f75a3582d394bd
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-06-21 00:28:35 -0500

    first commit

---
+++ src/main/java/com/caichihua/pizzeria/DemoPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.caichihua.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caichihua/pizzeria/DemoPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caichihua.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
