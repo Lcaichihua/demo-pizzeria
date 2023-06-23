bb547cbdbf132b18ffd6c2ca26842a1c6a67091a
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-06-22 23:57:02 -0500

    add controller to idPizza

--- src/main/java/com/caichihua/pizzeria/web/controller/PizzaController.java
+++ src/main/java/com/caichihua/pizzeria/web/controller/PizzaController.java
@@ -5,6 +5,7 @@ import com.caichihua.pizzeria.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,9 @@ private final PizzaService pizzaService;
     @GetMapping
     public ResponseEntity<List<PizzaEntity>> getAll(){
         return ResponseEntity.ok(this.pizzaService.getAll());
+    }
+    @GetMapping("/idpizza")
+    public ResponseEntity<PizzaEntity> getAll(@PathVariable int idPizza){
+        return ResponseEntity.ok(this.pizzaService.get(idPizza));
     }
 }
